2bb4b21750e916c11dde1dd44fe193485a4b6bbe
nqdu <nqdu@foxmail.com>
2023-10-11 17:09:44 +0800

    delete useless files

--- compile_transpose.sh
+++ compile_transpose.sh
@@ -4,4 +4,4 @@ MPICXX=mpic++
 includes="-I./src -I$PH5_PATH/include -I$EIGEN_INC"
 set -x
 # transpose1 
-$MPICXX -DUSE_MPI src/transpose1.cpp -O3 -o ./bin/transpose $includes  -L$PH5_PATH/lib -lhdf5 
+$MPICXX -DUSE_MPI src/transpose.cpp -O3 -o ./bin/transpose $includes  -L$PH5_PATH/lib -lhdf5 
