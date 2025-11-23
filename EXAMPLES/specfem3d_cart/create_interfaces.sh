9c507e38ffeb458a799e816f8c0ecb769626ac4f
nqdu <nqdu@foxmail.com>
2025-11-22 19:38:18 -0500

    yaml parser

--- EXAMPLES/specfem3d_cart/create_interfaces.sh
+++ EXAMPLES/specfem3d_cart/create_interfaces.sh
@@ -8,15 +8,5 @@
 # load your modules
 module load gcc openmpi python/3.9.8 parallel-hdf5/gcc-8.3.0
 
-# set your params
-axisem_data_dir=.. # like /path/to/axisem/SOLVER/ak135
-output_dir=..  # OUTPUT traction/displ/accel to this dir
-coordir=.. # wave_discon_*coordinates 
 
-# time window
-nt=1000
-dt=0.025
-t0=0.  # started from earthquake origin time
-UTM_ZONE=10
-
-mpirun -np 120 python ../../coupling_specfem.py  $axisem_data_dir $coordir $UTM_ZONE $t0 $dt $nt $outdir
+mpirun -np 120 python ../../run_coupling.py $param.yaml
